5aca7c915fe44d8505a822689e786c2a26f36770
tpny <tonylulyl@gmail.com>
2023-02-10 04:29:39 -0500

    allow multiple player to play for multiple teams

--- main.py
+++ main.py
@@ -23,12 +23,58 @@ CALLAHAN = 7
 STATS_COUNT = 8
 
 #static
-teams_to_id = {}
-players_to_id = {}
 id_to_teams = {}
+teams_to_id = {}
 id_to_players = {}
-team_id_to_players = {}
+team_id_to_player_ids = {}
+base_players_to_id = {}
 
+def update_db_player_stats():
+  unprocessed_plays = db_connector.get_plays(unprocessed = True)
+  for unprocessed_play in unprocessed_plays:
+    team_id = unprocessed_play[0]
+    game_id = unprocessed_play[1]
+    plays_str = unprocessed_play[2]
+    players_to_id = {}
+    for players_in_team in db_connector.get_players(team_id):
+      players_to_id[players_in_team[1]] = players_in_team[0]
+    player_stats, invalid_moves = compute_play_stats(plays_str.split(","), players_to_id)
+    db_connector.insert_or_update_player_stats(player_stats, players_to_id, team_id, unprocessed_play[1])
+    db_connector.insert_or_update_play(team_id, game_id, plays_str, processed = True)
+    
+def refresh_globals():
+  global id_to_teams
+  global teams_to_id
+  id_to_teams = {}
+  teams_to_id = {}
+
+  for team_id, team_name in db_connector.get_teams():
+    team_id = str(team_id)
+
+    id_to_teams[team_id] = team_name
+    teams_to_id[team_name] = team_id
+
+  global id_to_players
+  global team_id_to_player_ids
+  global base_players_to_id
+  id_to_players = {}
+  team_id_to_player_ids = {}
+  base_players_to_id = {}
+
+
+  for player_id, player_name, team_id, player_base_id in db_connector.get_players():
+    player_id = str(player_id)
+
+    if(not player_base_id):
+      base_players_to_id[player_name] = player_id
+
+    id_to_players[player_id] = player_name
+
+    if(team_id): # this player is associated with a team
+      team_id = str(team_id)
+      if(team_id not in team_id_to_player_ids):
+        team_id_to_player_ids[team_id] = set()
+      team_id_to_player_ids[team_id].add(player_id)
 
 def compute_play_stats(play_seq, players_in_team):
   player_stats = {}
@@ -47,6 +93,7 @@ def compute_play_stats(play_seq, players_in_team):
           player_stats[player][BLOCK] -= 1
 
     else: # is not player
+      print(player, player_stats)
       player = one_prior
       if(play == "DEFENSE"):
         player_stats[player][BLOCK] += 1
@@ -81,33 +128,6 @@ def compute_play_stats(play_seq, players_in_team):
 
   return player_stats, invalid_moves
 
-@APP.route('/record/<team_name>_<game_id>', methods = ["POST", "GET"])
-def record(team_name, game_id):
-  global teams_to_id
-  global team_id_to_players
-
-  team_id = teams_to_id[team_name]
-
-  play = db_connector.get_plays(team_id = team_id, game_id = game_id)
-  plays = []
-  if(play): #not empty
-      plays = play[2].split(",")
-  if(request.method == "POST"):
-    action_type = request.form.get("button")
-    play = db_connector.get_plays(team_id = team_id, game_id = game_id)
-    if(action_type == "UNDO"):
-      del plays[-1]
-    else:
-      plays.append(action_type)
-    plays_str = ",".join(plays)
-    db_connector.insert_or_update_play(team_id = team_id, game_id = game_id, plays_str = plays_str, processed = False)
-
-  team_players = team_id_to_players[teams_to_id[team_name]]
-  player_stats, invalid_moves = compute_play_stats(plays, team_players)
-
-  return render_template("record.html", players = sorted(team_players), plays = reversed(plays[-10:]), actions = ACTIONS, invalid_moves = invalid_moves, stats_header = STATS, player_stats = player_stats, hide_stats = "" if player_stats else "hidden", disable_undo = len(plays) == 0)
-
-
 @APP.route('/')
 @APP.route('/index')
 def index():
@@ -121,6 +141,55 @@ def index():
   for game in games:
     game_info.append((game[0], team_dict[game[1]], team_dict[game[2]]))
   return render_template("index.html", game_info = game_info)
+
+@APP.route('/record/<team_id>_<game_id>', methods = ["POST", "GET"])
+def record(team_id, game_id):
+  global id_to_teams
+  global id_to_players
+  global team_id_to_player_ids
+  global base_players_to_id
+
+  team_id = str(team_id)
+  game_id = str(game_id)
+
+  play = db_connector.get_plays(team_id = team_id, game_id = game_id)
+  plays = []
+  if(play): #not empty
+      plays = play[2].split(",")
+  if(request.method == "POST"):
+    action_type = request.form.get("button")
+    print(action_type)
+    if(action_type == "UNDO"):
+      del plays[-1]
+    else:
+      plays.append(action_type)
+
+    plays_str = ",".join(plays)
+    db_connector.insert_or_update_play(team_id = team_id, game_id = game_id, plays_str = plays_str, processed = False)
+
+  player_stats, invalid_moves = compute_play_stats(plays, team_id_to_player_ids[team_id])
+
+  player_stats_in_names = {}
+  for player_id in player_stats:
+    player_stats_in_names[id_to_players[player_id]] = player_stats[player_id]
+
+  invalid_moves_in_name = set()
+  for invalid_move in invalid_moves:
+    if(invalid_move in id_to_players):
+      invalid_moves_in_name.add(id_to_players[invalid_move])
+    else:
+      invalid_moves_in_name.add(invalid_move)
+
+  plays_in_name = []
+  for play in reversed(plays[-10:]):
+    if(play in id_to_players):
+      plays_in_name.append(id_to_players[play])
+    else:
+      plays_in_name.append(play)
+
+  players_in_team = sorted(map(lambda player_id: (player_id, id_to_players[player_id]), team_id_to_player_ids[team_id]))
+  return render_template("record.html", team_name = id_to_teams[team_id], players_in_team = players_in_team, plays = plays_in_name, actions = ACTIONS, invalid_moves = invalid_moves_in_name, stats_header = STATS, player_stats = player_stats_in_names, hide_stats = "" if player_stats_in_names else "hidden", disable_undo = len(plays_in_name) == 0)
+
 
 @APP.route('/list_teams', methods = ["POST", "GET"])
 def list_teams():
@@ -139,63 +208,75 @@ def list_teams():
         err = "Team {} already exists".format(new_team)
       else:
         db_connector.create_team(new_team);
-        refresh_teams()
+        refresh_globals()
         return redirect(url_for("edit_team", team_name = new_team))
 
   return render_template("list_teams.html", teams_to_id = teams_to_id, error = err)
 
+
 @APP.route('/edit_team/<team_name>', methods = ["POST", "GET"])
 def edit_team(team_name):
-  global teams_to_id
   global id_to_teams
-  global players_to_id
+  global teams_to_id
+  global id_to_players
+  global team_id_to_player_ids
+  global base_players_to_id
 
   team_id = teams_to_id[team_name]
   err = ""
   if(request.method == "POST"):
     action_type = request.form.get("button")
-    if(action_type == "Add Player"):
+    if(action_type == "Create New Player"):
       new_player = request.form.get("player_name")
-      if(new_player in players_to_id):
-        err = "Player {} already registered with {}".format(new_player, id_to_teams[players_to_id[new_player][1]])
+      if(new_player in base_players_to_id):
+        err = "Player {} is already registered. ID: {}".format(new_player, base_players_to_id[new_player])
       else:
         db_connector.create_player(player_name = new_player, team_id = team_id);
-        refresh_players()
-    elif(action_type == "Remove Player"):
-      player_name = request.form.get("player_name")
-      print(player_name, players_to_id[player_name][0])
-      db_connector.remove_player(players_to_id[player_name][0])
-      refresh_players()
-    elif(action_type == "Rename Player"):
-      player_name = request.form.get("player_name")
-      new_player_name = request.form.get("new_name")
-      if(new_player_name in players_to_id):
-        err = "Player {} already registered with {}".format(new_player_name, id_to_teams[players_to_id[new_player_name][1]])
-      else:
-        db_connector.update_player(players_to_id[player_name][0], new_player_name, team_id)
-        refresh_players()
+        refresh_globals()
+    elif(action_type == "Add Existing Player"):
+      new_player = request.form.get("player_name")
+
+      db_connector.create_player(player_name = new_player, team_id = team_id, base_id = base_players_to_id[new_player]);
+      refresh_globals()
+    elif(action_type == "Remove Player From Team"):
+      player_id = request.form.get("player_id")
+      db_connector.remove_player(player_id)
+      refresh_globals()
     elif(action_type == "Transfer Player"):
-      player_name = request.form.get("player_name")
+      player_id = request.form.get("player_id")
       new_team_id = teams_to_id[request.form.get("new_team")]
-      db_connector.update_player(players_to_id[player_name][0], player_name, new_team_id)
-      refresh_players()
+      db_connector.update_player(player_id, new_team_id)
+      refresh_globals()
     elif(action_type == "View Stats"):
-      return redirect(url_for("view_player_stat", player_name = request.form.get("player_name")))
+      pass
+      # return redirect(url_for("view_player_stat", player_name = request.form.get("player_name")))
     elif(action_type == "Rename Team"):
       new_team_name = request.form.get("new_name")
       if(new_team_name in teams_to_id):
-        err = "Team {} already exists".format(new_team_name)
+        err = "Team {} is already registered. ID: {}".format(new_team_name, teams_to_id[new_team_name])
       else:
         db_connector.update_team(team_id = team_id, team_name = new_team_name)
-        refresh_teams()
+        refresh_globals()
         return redirect(url_for("edit_team", team_name = new_team_name))
 
-  players = []
-  for player_name in players_to_id:
-    if(players_to_id[player_name][1] == team_id):
-      players.append((player_name, players_to_id[player_name][0]))
+  team_players_with_id = []
+  team_players_name = set()
+  if(team_id in team_id_to_player_ids):
+    for player_id in team_id_to_player_ids[team_id]:
+      team_players_with_id.append((player_id, id_to_players[player_id]))
+      team_players_name.add(id_to_players[player_id])
 
-  return render_template("edit_team.html", curr_team_name = team_name, error = err, players = players, team_names = teams_to_id.keys())
+  base_players = []
+  for base_player_name in sorted(base_players_to_id.keys()):
+    if(base_player_name not in team_players_name):
+      base_players.append(base_player_name)
+
+  transferable_teams = []
+  for new_team in teams_to_id.keys():
+    if(new_team != team_name):
+      transferable_teams.append(new_team)
+
+  return render_template("edit_team.html", base_players = base_players, curr_team_name = team_name, error = err, players_with_id = sorted(team_players_with_id, key=lambda x: x[0]), team_names = transferable_teams)
 
 @APP.route('/list_games', methods = ["POST", "GET"])
 def list_games():
@@ -208,68 +289,62 @@ def list_games():
       game_time = request.form.get("game_time")
       note =request.form.get("note")
       if(home_team_id == away_team_id):
-        err = "Home and aways team cannot be the same"
+        err = "Home and away team cannot be the same"
       else:
-        # def create_game(self, home_team_id, away_team_id, game_time = None):
         db_connector.create_game(home_team_id = home_team_id, away_team_id = away_team_id, game_time = game_time)
     elif(action_type == "Start Record"):
-      team_name = id_to_teams[int(request.form.get("team_id"))]
-      game_id = int(request.form.get("game_id"))
-      print(team_name, game_id)
-      return redirect(url_for("record", team_name = team_name, game_id = game_id))
+      return redirect(url_for("record", team_id = request.form.get("team_id"), game_id = request.form.get("game_id")))
     
   return render_template("list_games.html", team_names = teams_to_id.keys(), games = db_connector.get_games(), id_to_teams = id_to_teams, error = err)
 
 
+@APP.route('/list_players', methods = ["POST", "GET"])
+def list_players():
+  global id_to_teams
+  global teams_to_id
+  global id_to_players
+  global team_id_to_player_ids
+  global base_players_to_id
+  
+  err = ""
+  if(request.method == "POST"):
+    action_type = request.form.get("button")
+    if(action_type == "Create New Player"):
+      new_player = request.form.get("player_name")
+      if(new_player in base_players_to_id):
+        err = "Player {} is already registered. ID: {}".format(new_player, base_players_to_id[new_player])
+      else:
+        db_connector.create_player(player_name = new_player, team_id = team_id);
+        refresh_globals()
+    elif(action_type == "Remove Player"):
+      player_id = request.form.get("player_id")
+      db_connector.remove_player(player_id, is_base = True)
+      refresh_globals()
+    elif(action_type == "Rename Player"):
+      player_name = request.form.get("player_name")
+      player_id = request.form.get("player_id")
+      new_name = request.form.get("new_name")
+      if(new_name in base_players_to_id):
+        err = "Player {} is already registered. ID: {}".format(new_name, base_players_to_id[new_name])
+      else:
+        db_connector.update_player(base_players_to_id[player_name], new_name)
+        refresh_globals()
+    elif(action_type == "View Stats"):
+      pass
+
+  return render_template("list_players.html", base_players_to_id = base_players_to_id, error = err)
+
 @APP.route('/view_team_stat/<team_name>', methods = ["POST", "GET"])
 def view_team_stat(team_name):
-  team_id = teams_to_id(team_name)
   return "TODO"
 
 @APP.route('/view_player_stat/<player_name>', methods = ["POST", "GET"])
 def view_player_stat(player_name):
-  player_id = players_to_id[player_name]
   return "TODO"
 
-def update_db_player_stats():
-  unprocessed_plays = db_connector.get_plays(unprocessed = True)
-  for unprocessed_play in unprocessed_plays:
-    team_id = unprocessed_play[0]
-    game_id = unprocessed_play[1]
-    plays_str = unprocessed_play[2]
-    players_to_id = {}
-    for players_in_team in db_connector.get_players(team_id):
-      players_to_id[players_in_team[1]] = players_in_team[0]
-    player_stats, invalid_moves = compute_play_stats(plays_str.split(","), players_to_id)
-    db_connector.insert_or_update_player_stats(player_stats, players_to_id, team_id, unprocessed_play[1])
-    db_connector.insert_or_update_play(team_id, game_id, plays_str, processed = True)
-    
-def refresh_teams():
-  global teams_to_id
-  global id_to_teams
-  teams_to_id = {}
-  id_to_teams = {}
-
-  for team_id, team_name in db_connector.get_teams():
-    teams_to_id[team_name] = team_id
-    id_to_teams[team_id] = team_name
-
-def refresh_players():
-  global players_to_id
-  global id_to_players
-  global team_id_to_players
-  players_to_id = {}
-  id_to_players = {}
-  team_id_to_players = {}
-
-  players_to_id = {}
-  for player_id, player_name, team_id in db_connector.get_players():
-    players_to_id[player_name] = (player_id, team_id)
-    id_to_players[player_id] = player_name
-    if(team_id not in team_id_to_players):
-      team_id_to_players[team_id] = set()
-    team_id_to_players[team_id].add(player_name)
-
+@APP.route('/view_game_stat/<game_id>', methods = ["POST", "GET"])
+def view_game_stat(game_id):
+  return "TODO"
 
 
 if __name__ == "__main__":
@@ -284,9 +359,9 @@ if __name__ == "__main__":
   print("get_games")
   print(db_connector.get_games())
 
-  refresh_teams()
-  refresh_players()
-  update_db_player_stats()
+  refresh_globals()
+  refresh_globals()
+  # update_db_player_stats()
 
   #db_connector.get_player_stats(game_id = None, player_id = None, team_id = None)
 
--- mysql_connector.py
+++ mysql_connector.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 class MySQLConnector:
-  def __init__(self, stats_list, user='root', host='localhost', password='lcpk1215', database='ultimate_stats'):
+  def __init__(self, stats_list, user='root', host='localhost', password='', database='ultimate_stats'):
     # For public server
     self.user = user
     self.password = password
@@ -18,7 +18,7 @@ class MySQLConnector:
     cursor.execute(query)
     self.cnx.commit()
     
-    query = "CREATE TABLE IF NOT EXISTS PLAYERS ( id int NOT NULL AUTO_INCREMENT, player_name varchar(20) NOT NULL, team int NOT NULL, PRIMARY KEY (id), FOREIGN KEY (team) REFERENCES TEAMS(id));"
+    query = "CREATE TABLE IF NOT EXISTS PLAYERS ( id int NOT NULL AUTO_INCREMENT, player_name varchar(20) NOT NULL, team int, base_id int, hidden BOOLEAN NOT NULL DEFAULT FALSE, PRIMARY KEY (id), FOREIGN KEY (team) REFERENCES TEAMS(id), FOREIGN KEY (base_id) REFERENCES PLAYERS(id));"
     cursor.execute(query)
     self.cnx.commit()
 
@@ -32,98 +32,107 @@ class MySQLConnector:
 
     stats_str = ",".join(map(lambda stats: " stats_{} int NOT NULL".format(stats), self.stats_list))
     query = "CREATE TABLE IF NOT EXISTS PLAYER_STATS (player int NOT NULL, game int NOT NULL, team int NOT NULL, {}, PRIMARY KEY (player, game), FOREIGN KEY (player) REFERENCES PLAYERS(id), FOREIGN KEY (team) REFERENCES TEAMS(id), FOREIGN KEY (game) REFERENCES GAMES(id));".format(stats_str)
-    cursor.execute(query)
     self.cnx.commit()
 
     cursor.close()
 
   def insert_or_update_play(self, team_id, game_id, plays_str, processed = False):
     cursor = self.cnx.cursor()
-    query = "REPLACE INTO PLAYS (team, game, play, processed) VALUES({}, {}, '{}', {});".format(team_id, game_id, plays_str, processed)
-    cursor.execute(query)
+    query = "REPLACE INTO PLAYS (team, game, play, processed) VALUES(%s, %s, '%s', %s);"
+    cursor.execute(query, (team_id, game_id, plays_str, processed))
     self.cnx.commit()
     cursor.close()
 
   def create_game(self, home_team_id, away_team_id, game_time = None):
     cursor = self.cnx.cursor()
     if(game_time):
-      query = "REPLACE INTO GAMES (home_team, away_team, game_time) VALUES({}, {}, '{}');".format(home_team_id, away_team_id, game_time)
+      query = "REPLACE INTO GAMES (home_team, away_team, game_time) VALUES(%s, %s, '%s');"
+      cursor.execute(query, (home_team_id, away_team_id, game_time))
     else:
-      query = "REPLACE INTO GAMES (home_team, away_team) VALUES({}, {});".format(home_team_id, away_team_id)
-    print(query)
-    cursor.execute(query)
+      query = "REPLACE INTO GAMES (home_team, away_team) VALUES(%s, %s);"
+      cursor.execute(query, (home_team_id, away_team_id))
     self.cnx.commit()
     cursor.close()
 
   def update_game(self, game_id, home_team_id, away_team_id, game_time = None):
     cursor = self.cnx.cursor()
     if(datetime):
-      query = "REPLACE INTO GAMES (home_team, away_team, game_time) VALUES({}, {}, {});".format(home_team_id, away_team_id, game_time)
+      query = "REPLACE INTO GAMES (home_team, away_team, game_time) VALUES(%s, %s, %s);"
+      cursor.execute(query, (home_team_id, away_team_id, game_time))
     else:
-      query = "REPLACE INTO GAMES (home_team, away_team) VALUES({}, {});".format(home_team_id, away_team_id)
-    cursor.execute(query)
+      query = "REPLACE INTO GAMES (home_team, away_team) VALUES(%s, %s);"
+      cursor.execute(query, (home_team_id, away_team_id))
     self.cnx.commit()
     cursor.close()
 
   # def remove_game(self, game_id)
   #   cursor = self.cnx.cursor()
-  #   query = "DELETE FROM GAMES WHERE id = {};".format(game_id)
+  #   query = "DELETE FROM GAMES WHERE id = %s;".format(game_id)
   #   cursor.execute(query)
   #   self.cnx.commit()
   #   cursor.close()
 
   def create_team(self, team_name):
     cursor = self.cnx.cursor()
-    query = "INSERT INTO TEAMS (team_name) VALUES('{}');".format(team_name)
-    cursor.execute(query)
+    query = "INSERT INTO TEAMS (team_name) VALUES(%s);"
+    cursor.execute(query, (team_name, ))
     self.cnx.commit()
     cursor.close()
 
   def update_team(self, team_id, team_name):
     cursor = self.cnx.cursor()
-    query = "UPDATE TEAMS SET team_name = '{}' WHERE id = {};".format(team_name, team_id)
-    cursor.execute(query)
+    query = "UPDATE TEAMS SET team_name = %s WHERE id = %s;"
+    print((team_name, team_id))
+    cursor.execute(query, (team_name, team_id))
     self.cnx.commit()
     cursor.close()
 
   # def remove_team(self, team_id)
   #   cursor = self.cnx.cursor()
-  #   query = "DELETE FROM TEAMS WHERE id = {};".format(team_id)
+  #   query = "DELETE FROM TEAMS WHERE id = %s;".format(team_id)
   #   cursor.execute(query)
   #   self.cnx.commit()
   #   cursor.close()
 
-  def create_player(self, player_name, team_id):
+  def create_player(self, player_name, team_id, base_id = "NULL"):
     cursor = self.cnx.cursor()
-    query = "INSERT INTO PLAYERS (player_name, team) VALUES('{}', {});".format(player_name, team_id)
+    query = "INSERT INTO PLAYERS (player_name, team, base_id) VALUES(%s, %s, %s);"
     print(query)
-    cursor.execute(query)
+    cursor.execute(query, (player_name, team_id, base_id))
     self.cnx.commit()
     cursor.close()
 
-  def update_player(self, player_id, player_name, team_id):
+  def update_player(self, player_id, player_name = None, team_id = None):
     cursor = self.cnx.cursor()
-    query = "UPDATE PLAYERS SET player_name = '{}', team = {} WHERE id = {};".format(player_name, team_id, player_id)
-    cursor.execute(query)
+    if(player_name):
+      query = "UPDATE PLAYERS SET player_name = %s WHERE id = %s OR base_id = %s;"
+      cursor.execute(query, (player_name, player_id, player_id))
+    if(team_id):
+      query = "UPDATE PLAYERS SET team = %s WHERE id = %s;"
+      cursor.execute(query, (team_id, player_id))
     self.cnx.commit()
     cursor.close()
 
-  def remove_player(self, player_id):
+  def remove_player(self, player_id, is_base = False):
     cursor = self.cnx.cursor()
-    query = "DELETE FROM PLAYERS WHERE id = {};".format(player_id)
-    cursor.execute(query)
-    query = "DELETE FROM PLAYER_STATS WHERE player = {};".format(player_id)
-    cursor.execute(query)
+    if(is_base): # hide base player and all its stats
+      query = "UPDATE PLAYERS SET hidden = TRUE WHERE base_id = %s or id = %s;"
+      cursor.execute(query, (player_id, player_id))
+    else: # remove a player from a team
+      query = "UPDATE PLAYERS SET team = %s WHERE id = %s;"
+      cursor.execute(query, ("NULL", player_id))
+
     self.cnx.commit()
     cursor.close()
 
-  def get_players(self, team_id = None):
+  def get_players(self, team_id = None, get_hidden = False):
     cursor = self.cnx.cursor()
     if(team_id):
-      query = "SELECT * FROM PLAYERS WHERE team = {};".format(team_id)
+      query = "SELECT * FROM PLAYERS WHERE team = %s AND hidden = %s;"
+      cursor.execute(query, (team_id, not get_hidden))
     else:
       query = "SELECT * FROM PLAYERS;"
-    cursor.execute(query)
+      cursor.execute(query)
     data = cursor.fetchall()
     cursor.close()
     return data
@@ -142,15 +151,16 @@ class MySQLConnector:
     if(team_id or game_id or unprocessed):
       criti = []
       if(team_id):
-        criti.append("team = {}".format(team_id))
+        criti.append("team = %s".format(team_id))
       if(game_id):
-        criti.append("game = {}".format(game_id))
+        criti.append("game = %s".format(game_id))
       if(unprocessed):
         criti.append("processed = FALSE")
-      query = "SELECT * FROM PLAYS WHERE {};".format(" AND ".join(criti))
+      query = "SELECT * FROM PLAYS WHERE %s;"
+      cursor.execute(query, " AND ".join(criti))
     else:
       query = "SELECT * FROM PLAYS;"
-    cursor.execute(query)
+      cursor.execute(query)
     data = None
     if(team_id and game_id):
       data = cursor.fetchone()
@@ -172,25 +182,35 @@ class MySQLConnector:
     if(game_id or player_id or team_id):
       criti = []
       if(game_id):
-        criti.append("game = {}".format(game_id))
+        criti.append("game = %s".format(game_id))
       if(player_id):
-        criti.append("player = {}".format(player_id))
+        criti.append("player = %s".format(player_id))
       if(team_id):
-        criti.append("team = {}".format(team_id))
-      query = "SELECT * FROM PLAYER_STATS WHERE {};".format(" AND ".join(criti))
+        criti.append("team = %s".format(team_id))
+      query = "SELECT * FROM PLAYER_STATS WHERE %s;"
+      cursor.execute(query, " AND ".join(criti))
     else:
       query = "SELECT * FROM PLAYER_STATS;"
-    cursor.execute(query)
+      cursor.execute(query)
     data = cursor.fetchall()
     cursor.close()
     return data
 
   def insert_or_update_player_stats(self, player_stats, players_to_id, team, game):
     cursor = self.cnx.cursor()
-    stats_str = ",".join(map(lambda stats: " stats_{}".format(stats), self.stats_list))
+    stats_str = ",".join(map(lambda stats: " stats_%s".format(stats), self.stats_list))
     for player in player_stats:
       player_stats_str = ",".join(map(str, player_stats[player]))
-      query = "REPLACE INTO PLAYER_STATS (player, game, team, {}) VALUES({}, {}, {}, {});".format(stats_str, players_to_id[player], game, team, player_stats_str)
-      cursor.execute(query)
+      query = "REPLACE INTO PLAYER_STATS (player, game, team, %s) VALUES(%s, %s, %s, %s);"
+      cursor.execute(query, (stats_str, players_to_id[player], game, team, player_stats_str))
     self.cnx.commit()
     cursor.close()
+
+
+  def execute_sql(self, query):
+    cursor = self.cnx.cursor()
+    cursor.execute(query)
+    self.cnx.commit()
+    data = cursor.fetchall()
+    cursor.close()
+    return data
